5e11ede97615f327642e4e888f50b9a25b69f322
fatihydrm <fatihydrm@outlook.com>
2021-10-07 02:08:56 +0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 cw.oninput = function () {
-  if (this.value.length > 3)
-      this.value = this.value.slice(0,3); 
+  if (this.value.length > 4)
+      this.value = this.value.slice(0,4); 
+}
+cardnumber.oninput = function () {
+  if (this.value.length > 16)
+      this.value = this.value.slice(0,16); 
+}
+cardholders.oninput = function () {
+  if (this.value.length > 40)
+      this.value = this.value.slice(0,40); 
 }
